29a4d895884ca412033ea986a5f0ecb1d60f1bce
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-01 01:22:50 +0200

    development

--- src/app/api/posts/[slug]/route.tsx
+++ src/app/api/posts/[slug]/route.tsx
@@ -1,11 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
-// import Pool from "../../../lib/db"
+import Pool from "../../../lib/db"
 
 export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
     try {
         const { slug } = params
+
+        const [rows] = await Pool.query(
+            "SELECT * from posts where slug = ?",
+            [slug]
+        );
+
+
+        if (rows.length === 0) {
+            return NextResponse.json({
+                error: "No posts found"
+            }, {status: 404})
+        }
+
         return NextResponse.json({
-            message: slug
+            message: "Post was found",
+            post: rows[0]
+        }, {status: 200})
+
+        return NextResponse.json({
         })
     } catch (err) {
         return NextResponse.json({
