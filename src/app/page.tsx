0d1a84f8e40bc09870b8193223579bf32015b85a
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-01 18:25:40 +0200

    development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const page = () => {
   return (
-    <section className="w-full h-screen bg-slate-950 flex flex-col gap-2 items-center justify-center">
+    <section className="w-full h-screen bg-slate-950 text-sky-50 flex flex-col gap-2 items-center justify-center">
       <h1 className="uppercase font-semibold">easy blog website</h1>
       <p>I just made it practicing how to use next js&apos;s backend and how to use slugs</p>
       <p>Here are some important links;</p>
